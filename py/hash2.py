7de4f3cf69e538e8d1544b65ba772df555e165b0
xrabcde <xrabcde@gmail.com>
2021-08-19 23:41:08 +0900

    add: 해시-전화번호 목록 다른풀이 추가

--- py/hash2.py
+++ py/hash2.py
@@ -1,10 +1,19 @@
 def solution(phone_book):
-    answer = True
     phone_book.sort()
 
     for i in range(len(phone_book)-1):
         if len(phone_book[i]) < len(phone_book[i+1]):
             if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
-                answer = False
-                break
-    return answer
+                return False
+
+    return True
+
+# zip과 startswith 활용한 코드
+def solution(phone_book):
+    phone_book.sort()
+
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+
+    return True
